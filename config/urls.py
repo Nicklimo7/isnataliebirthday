bd87c1cbb69aae3d813c66c2f2aca7f90e9b5d78
Nicholas Limoncelli <nicholas.limoncelli@rula.com>
2025-05-24 21:11:33 -0500

    Render blueprint

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
@@ -22,7 +23,8 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("Django on Render is working!!!!")
 
+
 urlpatterns = [
-    path("", home)
-    path('admin/', admin.site.urls),
+    path("", home),
+    path("admin/", admin.site.urls),
 ]
